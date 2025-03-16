c875d54acae6407b0302f3d3bbbab8c27f937adf
wlolly <wlolly@qq.com>
2025-03-16 15:42:23 +0000

    Restored to '8ca5f346248ba7746eba1d4a03b2031accc6b78c'
    Replit-Restored-To: 8ca5f346248ba7746eba1d4a03b2031accc6b78c

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,13 +1,14 @@
+import { defineConfig } from "drizzle-kit";
 
-import type { Config } from 'drizzle-kit';
-import dotenv from 'dotenv';
-dotenv.config();
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL, ensure the database is provisioned");
+}
 
-export default {
-  schema: './shared/schema.ts',
-  out: './migrations',
-  driver: 'pg',
+export default defineConfig({
+  out: "./migrations",
+  schema: "./shared/schema.ts",
+  dialect: "postgresql",
   dbCredentials: {
-    connectionString: process.env.DATABASE_URL || '',
+    url: process.env.DATABASE_URL,
   },
-} satisfies Config;
+});
--- shared/schema.ts
+++ shared/schema.ts
@@ -1,4 +1,4 @@
-import { pgTable, integer, varchar, boolean, timestamp, pgEnum, decimal, text } from "drizzle-orm/pg-core";
+import { mysqlTable, int, varchar, boolean, timestamp, mysqlEnum, decimal, text } from "drizzle-orm/mysql-core";
 import { createInsertSchema } from "drizzle-zod";
 import { z } from "zod";
 
